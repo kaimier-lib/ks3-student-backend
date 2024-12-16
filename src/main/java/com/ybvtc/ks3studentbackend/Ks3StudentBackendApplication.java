270b4476866f36e5bbdb391b08458a34b527c06c
开米尔 <kaimier@qq.com>
2024-12-16 09:40:49 +0800

    first commit

---
+++ src/main/java/com/ybvtc/ks3studentbackend/Ks3StudentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ybvtc.ks3studentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ks3StudentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ks3StudentBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ybvtc/ks3studentbackend/Ks3StudentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ybvtc.ks3studentbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ks3StudentBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
