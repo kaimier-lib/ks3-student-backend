4dabaec81f575886fbaf6aa777f8704ab10d5a0d
nasa <kobayashi154@163.com>
2025-01-03 10:26:51 +0800

    update:测试pr用，没有修改代码，加了注释

--- src/main/java/com/ybvtc/ks3studentbackend/Ks3StudentBackendApplication.java
+++ src/main/java/com/ybvtc/ks3studentbackend/Ks3StudentBackendApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Ks3StudentBackendApplication {
 
+    /**
+     * 程序的入口点
+     * 使用Spring Boot框架启动应用程序
+     *
+     * @param args 命令行参数，允许在启动时传递给应用程序
+     */
     public static void main(String[] args) {
         SpringApplication.run(Ks3StudentBackendApplication.class, args);
     }
